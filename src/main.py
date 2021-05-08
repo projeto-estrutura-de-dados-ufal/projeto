b045bc26b27a0e79206c46d49d2bc2b8c10e8993
ruifernandees <dev.ruifernandes@gmail.com>
2021-05-07 21:46:49 -0300

    Implement readData function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,31 @@
 import pandas as pd
+
+def readData():
+    airbnb = pd.read_csv('../data/airbnb.csv').to_dict()
+
+    matrix = []
+    for i in airbnb:
+        listValues = list(airbnb[i].values())
+        matrix.append(listValues)
+
+    allKeys = list(airbnb.keys())
+    myDict = {} 
+    counter = 0
+    while counter < len(matrix[0]):
+        tempList = {} 
+        j = 0
+        for i in range(len(matrix)):
+            if j != 0:
+                tempList[allKeys[i]] = matrix[i][counter]
+            j += 1
+
+        myDict[matrix[0][counter]] = tempList
+        counter += 1
+        
+    return myDict
+
+        
+if __name__ == "__main__":
+    myDict = readData()
+    for i in myDict:
+        print(i, myDict[i])
