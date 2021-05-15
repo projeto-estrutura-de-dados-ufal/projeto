a8505577cd3f9cb2236bfc4564633a33f4e67042
Luiz Antonio <luizantonio_guimaraes98@hotmail.com>
2021-05-14 22:12:31 -0300

    Create Queue Co-authored-by: jks010 <gabriellmm2013@gmail.com>

---
+++ src/StaticList/fila.py
@@ -0,0 +1,39 @@
+class Fila:
+    
+    def __init__(self):
+        self.fila = []
+        self.limit=10 
+        self.size = 0  
+        
+    def insert(self,value):
+        if(self.size >= self.limit):
+            return
+        self.fila.append(value)
+        self.size +=1        
+
+    def remove(self):
+        if (self.size == 0):
+            return False
+
+        del self.fila[0]
+        self.size-=1
+        return True
+
+    def tam(self):
+        print("O tamanho é:",self.size)
+
+    def search(self,value):
+
+        for i in range(self.size):
+
+            if("id",value)in self.fila[i].value():
+
+                return i
+        return -1
+
+
+        return -1
+
+    def show(self):
+        for i in range(self.size):
+            print(self.fila[i])
