f52c66b25760c620dda941dd34dfdd606a928ad1
ruifernandees <dev.ruifernandes@gmail.com>
2021-05-17 20:10:44 -0300

    Update env

--- src/env/env.py
+++ src/env/env.py
@@ -1,4 +1,4 @@
 env = {
-    'path_airbnb': "../data/airbnb-all-elements.csv",
-    'path_marvel': "../data/hero-network.csv"
+    'path_airbnb': "../../data/airbnb-all-elements.csv",
+    'path_marvel': "../../data/hero-network.csv"
 }
